a7cd3c17efc65290b1d720c9f3444bcfc3689c28
Wim Looman <ghostunderscore@gmail.com>
2011-06-24 02:57:08 +1200

    A simple example main file

--- src/comms/main.c
+++ src/comms/main.c
@@ -1,4 +1,23 @@
 #include "board.h"
+#include "common.h"
+
+#include "libs/led.h"
 
 int main(int argc, char *argv[]) {
+    led_init();
+
+    uint16_t num = 0x00;
+    uint32_t count = 0x01;
+
+    for (;;) {
+        led_display(num);
+        num = (num + 1) % 100;
+        count = 0x01;
+        while (count != 0x00) {
+            count++;
+            led_show();
+        }
+    }
+
+    return 0;
 }
