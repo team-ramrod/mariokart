38ac1ccc2f8955dbb87a97c85f469519503b8ea8
Henry Jenkins <henry@henryjenkins.name>
2011-06-29 18:17:59 +1200

    Removed random demo util

--- Software/lib/utility/demo-fw/pc-tools/CreateDemoBin/PPT/CreateDemoBinIFlash.bat
+++
@@ -1,2 +0,0 @@
-..\bin\CreateDemoBin  -width 320 -height 240 -bitdepth 8 -rotate 270 -noreversebitmaporder DEMOBINFLASH1
-pause
--- Software/lib/utility/demo-fw/pc-tools/CreateDemoBin/PPT/CreatelDemoBinV.bat
+++
@@ -1,2 +0,0 @@
-..\bin\CreateDemoBin  -width 240 -height 320 -bitdepth 16 -rotate 0 -ssPageNumber 17 --ssWidth 320 -ssHeight 240 -ssBitdepth 16 -ssRotate 270 SamDEMOV
-pause
