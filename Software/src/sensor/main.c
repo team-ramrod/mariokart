8119cebcab5cb4c9b651f370f5c7860195316d01
Henry Jenkins <henry@henryjenkins.name>
2011-07-31 15:31:44 +1200

    Changed to using all the atmel example code
    The main file now contains a modified version of what is in the getting
    started example project by atmel

--- Software/src/sensor/main.c
+++ Software/src/sensor/main.c
@@ -1,75 +1,181 @@
+/**
+ * This was stolen from the getting started project in the atmel project library
+ */
+
+//------------------------------------------------------------------------------
+//         Headers
+//------------------------------------------------------------------------------
 
 #include <board.h>
 #include <pio/pio.h>
-
-static const Pin pinsLeds[] = {PINS_LEDS};
-static const unsigned int numLeds = PIO_LISTSIZE(pinsLeds);
-
-unsigned char LED_Configure(unsigned int led) {
-    // Check that LED exists                                                                             
-    if (led >= numLeds) {                                                                                
-
-        return 0;                                                                                        
-    }                                                                                                    
-
-    // Configure LED
-    return (PIO_Configure(&pinsLeds[led], 1));                                                           
-}
+#include <pio/pio_it.h>
+#include <pit/pit.h>
+#include <aic/aic.h>
+#include <tc/tc.h>
+#include <utility/led.h>
 
 //------------------------------------------------------------------------------
-/// Turns the given LED on if it exists; otherwise does nothing.
-/// \param led  Number of the LED to turn on.
-/// \return 1 if the LED has been turned on; 0 otherwise.
+//         Local definitions
 //------------------------------------------------------------------------------
-unsigned char LED_Set(unsigned int led) {
-    // Check if LED exists
-    if (led >= numLeds) {
+#ifndef AT91C_ID_TC0
+#if defined(AT91C_ID_TC012)
+    #define AT91C_ID_TC0 AT91C_ID_TC012
+#elif defined(AT91C_ID_TC)
+    #define AT91C_ID_TC0 AT91C_ID_TC
+#else
+    #error Pb define ID_TC
+#endif
+#endif
 
-        return 0;
-    }
 
-    // Turn LED on
-    if (pinsLeds[led].type == PIO_OUTPUT_0) {
+/// PIT period value in µseconds.
+#define PIT_PERIOD          1000
 
-        PIO_Set(&pinsLeds[led]);
-    }
-    else {
+//------------------------------------------------------------------------------
+//         Local variables
+//------------------------------------------------------------------------------
 
-        PIO_Clear(&pinsLeds[led]);
-    }
+/// Indicates the current state (on or off) for each LED.
+unsigned char pLedStates[2] = {1, 1};
 
-    return 1;
-}
+/// Global timestamp in milliseconds since start of application.
+volatile unsigned int timestamp = 0;
 
-unsigned char LED_Clear(unsigned int led)
+//------------------------------------------------------------------------------
+//         Local functions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+/// Handler for PIT interrupt. Increments the timestamp counter.
+//------------------------------------------------------------------------------
+void ISR_Pit(void)
 {
-    // Check if LED exists
-    if (led >= numLeds) {
+    unsigned int status;
 
-        return 0;
+    // Read the PIT status register
+    status = PIT_GetStatus() & AT91C_PITC_PITS;
+    if (status != 0) {
+
+        // Read the PIVR to acknowledge interrupt and get number of ticks
+        timestamp += (PIT_GetPIVR() >> 20);
     }
-
-    // Turn LED off
-    if (pinsLeds[led].type == PIO_OUTPUT_0) {
-
-        PIO_Clear(&pinsLeds[led]);
-    }
-    else {
-
-        PIO_Set(&pinsLeds[led]);
-    }
-
-    return 1;
 }
 
-int main(int argc, char *argv[]) {
+//------------------------------------------------------------------------------
+/// Configure the periodic interval timer to generate an interrupt every
+/// millisecond.
+//------------------------------------------------------------------------------
+void ConfigurePit(void)
+{
+    // Initialize the PIT to the desired frequency
+    PIT_Init(PIT_PERIOD, BOARD_MCK / 1000000);
 
+    // Configure interrupt on PIT
+    AIC_DisableIT(AT91C_ID_SYS);
+    AIC_ConfigureIT(AT91C_ID_SYS, AT91C_AIC_PRIOR_LOWEST, ISR_Pit);
+    AIC_EnableIT(AT91C_ID_SYS);
+    PIT_EnableIT();
+
+    // Enable the pit
+    PIT_Enable();
+}
+
+//------------------------------------------------------------------------------
+/// Configures LEDs \#1 and \#2 (cleared by default).
+//------------------------------------------------------------------------------
+void ConfigureLeds(void)
+{
     LED_Configure(0);
     LED_Configure(1);
-
-    while(1) {
-        LED_Clear(0);
-        LED_Set(1);
-    }
-    return 0;
 }
+
+//------------------------------------------------------------------------------
+/// Interrupt handler for TC0 interrupt. Toggles the state of LED\#2.
+//------------------------------------------------------------------------------
+void ISR_Tc0(void)
+{
+    // Clear status bit to acknowledge interrupt
+    AT91C_BASE_TC0->TC_SR;
+
+    // Toggle LED state
+    LED_Toggle(1);
+}
+
+//------------------------------------------------------------------------------
+/// Configure Timer Counter 0 to generate an interrupt every 250ms.
+//------------------------------------------------------------------------------
+void ConfigureTc(void)
+{
+    unsigned int div;
+    unsigned int tcclks;
+
+    // Enable peripheral clock
+    AT91C_BASE_PMC->PMC_PCER = 1 << AT91C_ID_TC0;
+
+    // Configure TC for a 4Hz frequency and trigger on RC compare
+    TC_FindMckDivisor(4, BOARD_MCK, &div, &tcclks);
+    TC_Configure(AT91C_BASE_TC0, tcclks | AT91C_TC_CPCTRG);
+    AT91C_BASE_TC0->TC_RC = (BOARD_MCK / div) / 4; // timerFreq / desiredFreq
+
+    // Configure and enable interrupt on RC compare
+    AIC_ConfigureIT(AT91C_ID_TC0, AT91C_AIC_PRIOR_LOWEST, ISR_Tc0);
+    AT91C_BASE_TC0->TC_IER = AT91C_TC_CPCS;
+    AIC_EnableIT(AT91C_ID_TC0);
+
+    // Start the counter if LED is enabled.
+    if (pLedStates[1]) {
+
+        TC_Start(AT91C_BASE_TC0);
+    }
+}
+
+//------------------------------------------------------------------------------
+/// Waits for the given number of milliseconds (using the timestamp generated
+/// by the PIT).
+/// \param delay  Delay to wait for, in milliseconds.
+//------------------------------------------------------------------------------
+void Wait(unsigned long delay)
+{
+    volatile unsigned int start = timestamp;
+    unsigned int elapsed;
+    do {
+        elapsed = timestamp;
+        elapsed -= start;
+    }
+    while (elapsed < delay);
+}
+
+//------------------------------------------------------------------------------
+//         Exported functions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+/// Application entry point. Configures the DBGU, PIT, TC0, LEDs and buttons
+/// and makes LED\#1 blink in its infinite loop, using the Wait function.
+/// \return Unused (ANSI-C compatibility).
+//------------------------------------------------------------------------------
+int main(void)
+{
+    // Configuration
+    ConfigurePit();
+    ConfigureTc();
+    ConfigureLeds();
+    
+    // Main loop
+    while (1) {
+
+        // Wait for LED to be active
+        while (!pLedStates[0]);
+        
+        // Toggle LED state if active
+        if (pLedStates[0]) {
+
+            LED_Toggle(0);
+        }
+
+        // Wait for 500ms
+        Wait(500);
+    }
+}
+
+
