abfb6935ac0442c4d4272dc18a9aee7a746f1570
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2011-08-07 15:13:47 +1200

    done as much as I can on the brakes without the actuator

--- Software/src/steering/main.c
+++ Software/src/steering/main.c
@@ -3,26 +3,39 @@
 #include <peripherals/pio/pio.h>
 #include <char_display.h>
 
-int min_angle;
-int max_angle;
-int pulses;
+//angle in degrees that the wheel may come within the end of its travel
+//due to some round processes in converting from pulses to degrees this 
+//must not be set to 0
+#define SAFTEY_MARGIN 10
+
+//minimum angle wheel can be turned to in degrees
+int steering_min_angle;
+
+//maximum angle wheel can be turned to in degrees
+int steering_max_angle;
+
+//location of wheel measured in pulses from encoder
+int steering_loc_in_pulses = 0;
 
 //sets steering to an angle (in degrees) between min_angle and max_angle
 void set_steering(int angle){
 
-    if(angle > max_angle){
-        angle = max_angle;
+    if(angle > steering_max_angle - SAFTEY_MARGIN){
+        angle = steering_max_angle;
     }
-    else if(angle < min_angle){
-        angle = min_angle;
+    else if(angle < steering_min_angle + SAFTEY_MARGIN){
+        angle = steering_min_angle;
     }
     //convert angle to pulses and account for any offset
-    pulses = ((angle + max_angle + min_angle) * ENCODER_PULSES_PER_REV)/360 ;
+    steering_loc_in_pulses = ((angle + steering_max_angle + steering_min_angle)
+            * ENCODER_PULSES_PER_REV)/360 ;
 }
 
-//calibrates steering position
+//calibrates steering position by finding locaiton of limit switches and
+//assuming straight ahead is halfway between them
 void cal_steering(void){
 
+    //sets pins of limit switches
     const Pin pin_lim_up = LIM_SW_UP;
     const Pin pin_lim_down = LIM_SW_DOWN;
 
@@ -32,9 +45,11 @@ void cal_steering(void){
         continue;
     }
 
+    //stop motor
     act_driver_drive(0);
 
-    min_angle = 360 * encoder_position_output / ENCODER_PULSES_PER_REV;
+    //record value in degrees
+    steering_min_angle = 360 * encoder_position_output / ENCODER_PULSES_PER_REV;
 
     //drive steering clockwise until limit hit
     act_driver_drive(50);
@@ -42,15 +57,17 @@ void cal_steering(void){
         continue;
     }
 
+    //stop motor
     act_driver_drive(0);
 
-    max_angle = 360 * encoder_position_output / ENCODER_PULSES_PER_REV;
+    //record value in degrees
+    steering_max_angle = 360 * encoder_position_output / ENCODER_PULSES_PER_REV;
+
+    //drive motor to center position
+    set_steering(0);
 }
 
 int main(int argc, char *argv[]) {
-
-    int speed;
-    pulses = 0;
 
     //enables interrupts (note resets all configured interrupts)
     PIO_InitializeInterrupts(AT91C_AIC_PRIOR_LOWEST);
@@ -64,15 +81,15 @@ int main(int argc, char *argv[]) {
     //cal_steering();
  
     //replaces what calibrating does (these 2 lines should not be in final code)
-    min_angle = -270;
-    max_angle = 270;
+    steering_min_angle = -270;
+    steering_max_angle = 270;
 
-    //sets positon to turn wheel to (used until can bus in place)
+    //sets positon to turn wheel to in degrees (used until can bus in place)
     set_steering(200);
 
     //infinite loop running PID controller
     while(1){
-        speed = act_driver_pid(pulses, encoder_position_output);
+        int speed = act_driver_pid(steering_loc_in_pulses, encoder_position_output);
         act_driver_drive(speed);
     }
     
