bcd77b4e40f6f0b198c0678dcba91832e14ae6b6
Henry Jenkins <henry@henryjenkins.name>
2011-08-27 14:33:00 +1200

    Added comment about allowed values for CAN bus speed

--- Software/lib/boards/mariokartv1/board.h
+++ Software/lib/boards/mariokartv1/board.h
@@ -223,7 +223,7 @@
 #define PINS_ADC PIN_ADC0_ADC0, PIN_ADC0_ADC1, PIN_ADC0_ADC2, PIN_ADC0_ADC3
 */
 
-// CAN Bus transfer rate.
+// CAN Bus transfer rate. Allowed values: 1000, 800, 500, 250, 125, 50, 25, 10
 #define CAN_BUS_SPEED 1000
 // CAN Status LED #0 pin definition.
 #define PIN_CAN_LED_0  {AT91C_PIO_PB10, AT91C_BASE_PIOB, AT91C_ID_PIOB, PIO_OUTPUT_0, PIO_DEFAULT}
