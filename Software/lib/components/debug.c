be7b3f612ffdf2cf332d009e5db5214ce7780ca4
Henry Jenkins <henry@henryjenkins.name>
2011-08-28 15:52:05 +1200

    Changed to using a variable for the software name
    This is so it can be passed in to the single init funciton

--- Software/lib/components/debug.c
+++ Software/lib/components/debug.c
@@ -9,10 +9,6 @@
 //------------------------------------------------------------------------------
 #include "debug.h"
 
-#ifndef SOFTWARE_NAME
-    #error You must define variable SOFTWARE_NAME!!
-#endif
-
 //------------------------------------------------------------------------------
 //         Global variable init
 //------------------------------------------------------------------------------
@@ -24,10 +20,10 @@
 //------------------------------------------------------------------------------
 //         Global Functions
 //------------------------------------------------------------------------------
-void debug_init(){
+void debug_init(char *software_name){
     TRACE_CONFIGURE(DBGU_STANDARD, DEBUG_SPEED, BOARD_MCK);   
     //printf("-- Mariokart - %s Board %s --\n\r", SOFTWARE_NAME, SOFTPACK_VERSION);
-    printf("-- Mariokart - %s Board %s --\n\r", "SOFTWARE_NAME", SOFTPACK_VERSION);
+    printf("-- Mariokart - %s Board %s --\n\r", software_name, SOFTPACK_VERSION);
     printf("-- %s\n\r", BOARD_NAME);
     printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
 }
