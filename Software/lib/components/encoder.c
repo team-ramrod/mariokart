35540a621d46a2975b939c579275fee9a2e12681
Henry Jenkins <henry@henryjenkins.name>
2011-08-30 14:51:48 +1200

    Fixed a broken cast
    Casts should be avoided at all costs.
    I also made the pins a local variable since that is the only place they
    are being called from

--- Software/lib/components/encoder.c
+++ Software/lib/components/encoder.c
@@ -3,30 +3,30 @@
 #include <peripherals/pio/pio_it.h>
 #include <boards/mariokartv1/board.h>
 
-/// Pio pins for encoder
-const Pin encoder_pin_cw = ENCODER_CLOCKWISE;
-const Pin encoder_pin_acw = ENCODER_ANTICLOCKWISE;
-
 //updates position value if turning clockwise
-void ENCODER_ISR_UP ( void )
+void ENCODER_ISR_UP (const Pin *encoder_pin_cw )
 {
 //only updates on change from high to low
-    if(!PIO_Get(&encoder_pin_cw)){
+    if(!PIO_Get(encoder_pin_cw)){
         encoder_position_output++;
     }
 }
 
 //updates position value if turning anticlockwise
-void ENCODER_ISR_DOWN ( void )
+void ENCODER_ISR_DOWN ( const Pin *encoder_pin_acw )
 {
 //only updates on change from high to low
-    if(!PIO_Get(&encoder_pin_acw)){
+    if(!PIO_Get(encoder_pin_acw)){
         encoder_position_output--;
     }
 }
 
 //Sets up pins for encoder 
 void encoder_init(void){
+    // Pio pins for encoder
+    const Pin encoder_pin_cw = ENCODER_CLOCKWISE;
+    const Pin encoder_pin_acw = ENCODER_ANTICLOCKWISE;
+
 
     //initilise pulse counter
     encoder_position_output = 0;
@@ -36,8 +36,8 @@ void encoder_init(void){
     PIO_Configure(&encoder_pin_acw, 1);
 
     // Initialize interrupts
-    PIO_ConfigureIt(&encoder_pin_cw, (void (*)(const Pin *)) ENCODER_ISR_UP);
-    PIO_ConfigureIt(&encoder_pin_acw, (void (*)(const Pin *)) ENCODER_ISR_DOWN);
+    PIO_ConfigureIt(&encoder_pin_cw, ENCODER_ISR_UP);
+    PIO_ConfigureIt(&encoder_pin_acw, ENCODER_ISR_DOWN);
     PIO_EnableIt(&encoder_pin_cw);
     PIO_EnableIt(&encoder_pin_acw);
 }
