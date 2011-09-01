2f6ec60b2359ec35aac69046c2c2740fc193880e
Wim Looman <ghostunderscore@gmail.com>
2011-09-01 16:05:32 +1200

    Added initial implementation of the protocol msg buffer

--- Software/lib/drivers/protocol/proto_msg_buff.c
+++ Software/lib/drivers/protocol/proto_msg_buff.c
@@ -1,2 +1,52 @@
 #include "proto_msg_buff.h"
 
+#include <utility/atomic.h>
+
+#define BUFFER_SIZE 10
+#define increment(pos) ((pos + 1) % BUFFER_SIZE)
+
+message_t proto_msg_buff[BUFFER_SIZE] = { { 0x0 } };
+
+volatile unsigned int proto_msg_buff_read  = 0,
+                      proto_msg_buff_write = 0;
+
+volatile unsigned int proto_msg_buff_mutex = unlocked;
+
+unsigned int proto_msg_buff_length() {
+    unsigned int tmp = 0x0;
+    lock_mutex(proto_msg_buff_mutex);
+        tmp = ((proto_msg_buff_write - proto_msg_buff_read) + BUFFER_SIZE) % BUFFER_SIZE;
+
+    unlock_mutex(proto_msg_buff_mutex);
+}
+
+message_t proto_msg_buff_pop() {
+    message_t tmp = { 0x0 };
+
+    lock_mutex(proto_msg_buff_mutex);
+        if (proto_msg_buff_read != proto_msg_buff_write) {
+            tmp = proto_msg_buff[proto_msg_buff_read];
+            proto_msg_buff_read = increment(proto_msg_buff_read);
+        }
+    unlock_mutex(proto_msg_buff_mutex);
+}
+
+message_t proto_msg_buff_peek() {
+    message_t tmp = { 0x0 };
+
+    lock_mutex(proto_msg_buff_mutex);
+        if (proto_msg_buff_read != proto_msg_buff_write) {
+            tmp = proto_msg_buff[proto_msg_buff_read];
+        }
+    unlock_mutex(proto_msg_buff_mutex);
+}
+
+bool proto_msg_buff_push(message_t msg) {
+    lock_mutex(proto_msg_buff_mutex);
+        if (proto_msg_buff_read == increment(proto_msg_buff_write)) {
+            proto_msg_buff_read = increment(proto_msg_buff_read);
+        }
+        proto_msg_buff[proto_msg_buff_write] = msg;
+        proto_msg_buff_write = increment(proto_msg_buff_write);
+    unlock_mutex(proto_msg_buff_mutex);
+}
