6725fed51db7c4b337b52c11c044cf9fa20e671c
Henry Jenkins <henry@henryjenkins.name>
2011-09-02 14:26:45 +1200

    Simons changes he made on his lunch break

--- Software/src/test_proto/client.c
+++ Software/src/test_proto/client.c
@@ -9,17 +9,17 @@ void main(void) {
     // initialise stuff
     proto_init();
 
-    
-    switch (proto_state()) {
-        case WAITING: // waiting for comms board to give the go ahead to calibrate
-            proto_wait();
-            // run calibration routine();
-            break;
-        case INITIALISING: // Waiting for all boards to finish calibration
-            proto_comms_wait();
-            break;
-        case RUNNING: // Normal state
-            while (1) {
+
+    while (1) {
+        switch (proto_state()) {
+            case WAITING: // waiting for comms board to give the go ahead to calibrate
+                proto_wait();
+                break;
+            case INITIALISING: // Waiting for all boards to finish calibration
+                // run calibration routine();
+                proto_comms_wait();
+                break;
+            case RUNNING: // Normal state
                 // if (incoming data) {
                 //     if (data is valid) {
                 //         setpoint = data;
@@ -30,14 +30,12 @@ void main(void) {
                 // }
                 // run iteration of PID loop using setpoint
                 // Any issues => state = ERROR; break;
-            }
-            break;
-        default: // ERROR
-            while(1) {
+                break;
+            default: // ERROR
                 //broadcast ERROR signal
                 // If reset signal received 
                 // then transition to WAITING state.
-            }
-            break;
+                break;
+        }
     }
 }
