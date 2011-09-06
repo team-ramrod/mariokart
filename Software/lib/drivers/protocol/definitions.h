8e1c2cc054e0504e395cefbd1844a19bd10ae058
Wim Looman <ghostunderscore@gmail.com>
2011-09-06 13:43:14 +1200

    Using Tabularize to align stuff

--- Software/lib/drivers/protocol/definitions.h
+++ Software/lib/drivers/protocol/definitions.h
@@ -14,37 +14,37 @@
 
 // Needs to be only 3 bit
 typedef enum {
-    ADDR_ERROR_RX     = 0x0,
-    ADDR_BROADCAST_RX = 0x1,
-    ADDR_BROADCAST_TX = 0x2,
-    ADDR_BRAKE        = 0x3,
-    ADDR_COMMS        = 0x4,
-    ADDR_COMMS_USB    = 0x4,
-    ADDR_STEERING     = 0x5,
-    ADDR_MOTOR        = 0x6,
-    ADDR_SENSOR       = 0x7,
+    ADDR_ERROR_RX         = 0x0,
+    ADDR_BROADCAST_RX     = 0x1,
+    ADDR_BROADCAST_TX     = 0x2,
+    ADDR_BRAKE            = 0x3,
+    ADDR_COMMS            = 0x4,
+    ADDR_COMMS_USB        = 0x4,
+    ADDR_STEERING         = 0x5,
+    ADDR_MOTOR            = 0x6,
+    ADDR_SENSOR           = 0x7,
 } address_t;
 
 // Needs to be only 8 bit
 typedef enum {
-    CMD_NONE  = 0x0,
-    CMD_GET   = 0x1,
-    CMD_REPLY = 0x2,
-    CMD_SET   = 0x3,
-    CMD_REQ_CALIBRATE = 0x04,
-    CMD_ACK_CALIBRATE = 0x05,
-    CMD_REQ_RUN = 0x06,
-    CMD_ACK_RUN = 0x07,
-    CMD_NO = 0x08,
-    CMD_ERROR = 0x09,
-    CMD_RUN = 0x0A,
-    CMD_CALIBRATE = 0x0B,
+    CMD_NONE              = 0x0,
+    CMD_GET               = 0x1,
+    CMD_REPLY             = 0x2,
+    CMD_SET               = 0x3,
+    CMD_REQ_CALIBRATE     = 0x04,
+    CMD_ACK_CALIBRATE     = 0x05,
+    CMD_REQ_RUN           = 0x06,
+    CMD_ACK_RUN           = 0x07,
+    CMD_NO                = 0x08,
+    CMD_ERROR             = 0x09,
+    CMD_RUN               = 0x0A,
+    CMD_CALIBRATE         = 0x0B,
 } command_t;
 
 typedef enum {STARTUP, CALIBRATING, RUNNING, ERROR} state_t;
 
 typedef enum {
-    VAR_SPEED = 0x1,
+    VAR_SPEED   = 0x1,
     VAR_BRK_POS = 0x2,
 } variable_t;
 
