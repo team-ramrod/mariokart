78fdf57a639ee0bdeda57076f21adccfc4c031fb
Zachary Taylor <zjt14@uclive.ac.nz>
2011-09-06 15:21:38 +1200

    adding basic kart location

--- Software/cv/build/Debug/GNU-Linux-x86/calibrate.o.d
+++
@@ -1,1 +0,0 @@
-build/Debug/GNU-Linux-x86/calibrate.o: calibrate.cpp
--- Software/cv/build/Debug/GNU-Linux-x86/main.o.d
+++ Software/cv/build/Debug/GNU-Linux-x86/main.o.d
@@ -1,1 +1,5 @@
-build/Debug/GNU-Linux-x86/main.o: main.cpp
+build/Debug/GNU-Linux-x86/main.o: main.cpp chessboard.h location.h
+
+chessboard.h:
+
+location.h:
