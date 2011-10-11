7d98a46a738959c0182c189c2273164ec4b727bd
Wim Looman <ghostunderscore@gmail.com>
2011-10-12 11:34:22 +1300

    Small cleanup

--- Software/client.py
+++ Software/client.py
@@ -7,7 +7,7 @@ from definitions import *
 if len(argv) > 1 and argv[1] in ('-h', '--help'):
     print 'Usage: %s [serial device]\n\tDefault device is /dev/ttyACM0\n' % (argv[0])
     exit(0)
-    
+
 cmd_re = compile(r'send\s+(\w+)\s+(\w+)\s*([\w\s]*)')
 
 serial = Serial()
@@ -40,7 +40,7 @@ while running:
                     if result:
                         print 'Received [%d] for [%s].\n' % (ord(result), data.split()[1])
                     else:
-                        print 'Reception times out.\n'
+                        print 'Reception timed out.\n'
             except Exception, e:
                 stderr.write('Incorrect command\n')
 
